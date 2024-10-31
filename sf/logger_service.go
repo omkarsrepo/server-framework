9b9339893d1f4ca8fad5ae94e46eed233093c7b4
USR9EQP <moreomkar@johndeere.com>
2024-11-01 00:24:51 +0530

    Refactored logger service to consider null ginCtx

--- sf/logger_service.go
+++ sf/logger_service.go
@@ -47,39 +47,72 @@ func LoggerServiceInstance() LoggerService {
 	return loggerServiceInstance
 }
 
-func (props *loggerService) Info(ginCtx *gin.Context) *zerolog.Event {
-	traceId := ginCtx.GetString("TRACE_ID")
-	logger := props.Logger.With().Str("traceId", traceId).Logger()
+func extractTraceId(ginCtx *gin.Context) (string, bool) {
+	traceId := ""
 
-	return logger.Info()
+	if ginCtx != nil {
+		traceId = ginCtx.GetString("TRACE_ID")
+		if traceId != "" {
+			return traceId, true
+		}
+	}
+
+	return traceId, false
+}
+
+func (props *loggerService) Info(ginCtx *gin.Context) *zerolog.Event {
+	traceId, ok := extractTraceId(ginCtx)
+	if ok {
+		logger := props.Logger.With().Str("traceId", traceId).Logger()
+
+		return logger.Info()
+	}
+
+	return props.Logger.Info()
 }
 
 func (props *loggerService) Error(ginCtx *gin.Context) *zerolog.Event {
-	traceId := ginCtx.GetString("TRACE_ID")
-	logger := props.Logger.With().Str("traceId", traceId).Logger()
+	traceId, ok := extractTraceId(ginCtx)
+	if ok {
+		logger := props.Logger.With().Str("traceId", traceId).Logger()
 
-	return logger.Error()
+		return logger.Error()
+	}
+
+	return props.Logger.Error()
 }
 
 func (props *loggerService) Err(ginCtx *gin.Context, err error) *zerolog.Event {
-	traceId := ginCtx.GetString("TRACE_ID")
-	logger := props.Logger.With().Str("traceId", traceId).Logger()
+	traceId, ok := extractTraceId(ginCtx)
+	if ok {
+		logger := props.Logger.With().Str("traceId", traceId).Logger()
 
-	return logger.Err(err)
+		return logger.Err(err)
+	}
+
+	return props.Logger.Err(err)
 }
 
 func (props *loggerService) Fatal(ginCtx *gin.Context) *zerolog.Event {
-	traceId := ginCtx.GetString("TRACE_ID")
-	logger := props.Logger.With().Str("traceId", traceId).Logger()
+	traceId, ok := extractTraceId(ginCtx)
+	if ok {
+		logger := props.Logger.With().Str("traceId", traceId).Logger()
 
-	return logger.Fatal()
+		return logger.Fatal()
+	}
+
+	return props.Logger.Fatal()
 }
 
 func (props *loggerService) Panic(ginCtx *gin.Context) *zerolog.Event {
-	traceId := ginCtx.GetString("TRACE_ID")
-	logger := props.Logger.With().Str("traceId", traceId).Logger()
+	traceId, ok := extractTraceId(ginCtx)
+	if ok {
+		logger := props.Logger.With().Str("traceId", traceId).Logger()
 
-	return logger.Panic()
+		return logger.Panic()
+	}
+
+	return props.Logger.Panic()
 }
 
 func (props *loggerService) GetZeroLogger() *zerolog.Logger {
